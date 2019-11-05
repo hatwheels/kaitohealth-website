ae5fd4366477e8c56c455664a898241589af47eb
hathweels <12772651+hatwheels@users.noreply.github.com>
2019-11-05 22:59:27 +0200

    font resizing

--- resources/js/components/Future.vue
+++ resources/js/components/Future.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="py-8">
+    <div class="py-12">
         <v-row v-if="$vuetify.breakpoint.mdAndUp" justify="space-around" align="center">
-            <v-col cols=5>
+            <v-col cols=5 justify="center" align="center">
                 <v-card
                     id="learnmore"
                     flat
                     color="transparent"
                 >
-                    <v-card-title class="accent--text display-1 font-weight-bold d-flex flex-column">
+                    <v-card-title class="accent--text d-flex flex-column">
                         <v-avatar size=60>
                             <v-img :src="learnmore.url" contain/>
                         </v-avatar>
-                        <p>{{ learnmore.title }}</p>
+                        <p class="mb-0 hidden-md-only font-weight-bold display-2 text-justify">{{ learnmore.title }}</p>
+                        <p class="mb-0 hidden-lg-and-up font-weight-bold display-1 text-justify">{{ learnmore.title }}</p>
                     </v-card-title>
-                    <v-card-text class="accent--text subtitle-1 text-left" v-html="learnmore.text"/>
+                    <v-card-text class="hidden-md-only accent--text headline text-justify" v-html="learnmore.text"/>
+                    <v-card-text class="hidden-lg-and-up accent--text title text-justify" v-html="learnmore.text"/>
                     <v-text-field
                         prepend-inner-icon="mdi-email"
                         append-outer-icon="mdi-send"
@@ -23,7 +25,7 @@
                     ></v-text-field>
                 </v-card>
             </v-col>
-            <v-col cols=5>
+            <v-col cols=5 justify="center" align="center">
                 <v-card
                     id="colla"
                     flat
@@ -33,15 +35,17 @@
                         <v-avatar size=60>
                             <v-img :src="collaborate.url" contain/>
                         </v-avatar>
-                        <p class="display-1 font-weight-bold">{{ collaborate.title }}</p>
+                        <p class="mb-0 hidden-md-only font-weight-bold display-2 text-justify">{{ collaborate.title }}</p>
+                        <p class="mb-0 hidden-lg-and-up font-weight-bold display-1 text-justify">{{ collaborate.title }}</p>
                     </v-card-title>
-                    <v-card-text class="accent--text body-1 text-left" v-html="collaborate.text"/>
+                    <v-card-text class="hidden-md-only accent--text headline text-justify" v-html="collaborate.text"/>
+                    <v-card-text class="hidden-lg-and-up accent--text title text-justify" v-html="collaborate.text"/>
                 </v-card>
             </v-col>
         </v-row>
         <div v-else>
             <v-row justify="space-around" align="center">
-                <v-col justify="center">
+                <v-col justify="center" align="center">
                     <v-card
                         id="learnmore"
                         flat
@@ -54,25 +58,24 @@
                                     contain
                                 />
                             </v-avatar>
-                            <v-card-title class="hidden-sm-and-down accent--text display-1 font-weight-bold">{{ learnmore.title }}</v-card-title>
-                            <v-card-title class="hidden-md-only accent--text headline font-weight-bold">{{ learnmore.title }}</v-card-title>
+                            <v-card-title class="accent--text headline font-weight-bold">{{ learnmore.title }}</v-card-title>
                         </v-row>
                         <v-card-text class="accent--text body-1 text-justify" v-html="learnmore.text"/>
                         <v-card width="400" color="transparent" flat>
-                        <v-text-field
-                            class="px-4"
-                            prepend-inner-icon="mdi-email"
-                            append-outer-icon="mdi-send"
-                            label="Your email"
-                            outlined
-                            @click:append-outer="{}"
-                        ></v-text-field>
+                            <v-text-field
+                                class="px-4"
+                                prepend-inner-icon="mdi-email"
+                                append-outer-icon="mdi-send"
+                                label="Your email"
+                                outlined
+                                @click:append-outer="{}"
+                            ></v-text-field>
                         </v-card>
                     </v-card>
                 </v-col>
             </v-row>
             <v-row justify="center" align="center">
-                <v-col>
+                <v-col justify="center" align="center">
                     <v-card
                         id="colla"
                         flat
@@ -85,8 +88,7 @@
                                     contain
                                 />
                             </v-avatar>
-                            <v-card-title class="hidden-sm-and-down accent--text display-1 font-weight-bold">{{ collaborate.title }}</v-card-title>
-                            <v-card-title class="hidden-md-only accent--text headline font-weight-bold">{{ collaborate.title }}</v-card-title>
+                            <v-card-title class="accent--text headline font-weight-bold">{{ collaborate.title }}</v-card-title>
                         </v-row>
                         <v-card-text class="accent--text body-1 text-justify" v-html="collaborate.text"/>
                     </v-card>
